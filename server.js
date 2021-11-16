2591e6f249526d1798401f35e74cbeb421e7d4ca
Valentin Röhle <valentin.roehle@join.de>
2021-11-16 14:30:22 +0100

    work in progress

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fs = require('fs')
 const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.write('Hello Node')
-    res.end()
-
+    res.writeHead(200, { 'Content-Type' : 'text/html' })
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error) {
