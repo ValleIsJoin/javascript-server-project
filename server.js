a09e6edf578209e33f7902f8e9e10caea503202f
Valentin Röhle <valentin.roehle@join.de>
2021-11-17 10:15:25 +0100

    finish with main design

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const fs = require('fs')
 const http = require('http')
+const fs = require('fs')
+const mime = require('mime-types')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/html' })
-    fs.readFile('index.html', function(error, data) {
+    res.writeHead(200, { 'Content-Type' : mime.lookup(req.url) })
+    fs.readFile(req.url.substr(1, req.url.length), function(error, data) {
         if (error) {
             res.writeHead(404)
             res.write('Error: File Not Found')
@@ -22,3 +23,4 @@ server.listen(port, function(error) {
         console.log('Server is listening on port ' + port)
     }
 })
+
